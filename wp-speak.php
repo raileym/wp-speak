5fadb687ae46adfdcf8d84df4ba8e2138420cd88
raileym <malcolm.railey@gmail.com>
2020-07-01 18:14:29 +0000

    My first commit.

---
+++ wp-speak.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wp Speak
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-speak
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Speak
+ */
+
+// Your code starts here.
